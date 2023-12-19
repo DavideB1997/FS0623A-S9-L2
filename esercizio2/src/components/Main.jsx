a386725993dd1d0698275b2e6cf24ecdc419b6c0
Davide Buglione <148747588+DavideB1997@users.noreply.github.com>
2023-12-19 16:43:56 +0100

    "piccolo fix"

--- esercizio2/src/components/Main.jsx
+++ esercizio2/src/components/Main.jsx
@@ -1,9 +1,4 @@
 import React from "react";
-import { Col, ListGroup, ListGroupItem, Row } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import Container from "react-bootstrap/Container";
-import books from "../Data/books.json";
 import Cards from "./Cards";
 
 class Main extends React.Component {
